2e498b1f0d780842d8ed1dc7e37e2c3174e3aa78
zhouliupku <pkulz@pku.edu.cn>
2022-04-04 13:01:02 -0400

    set up router and flash

--- schemas.js
+++ schemas.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
-        price: Joi.string().required(),
+        zoo: Joi.string().required(),
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
