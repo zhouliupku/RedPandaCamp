ea793e0c7d4f5192d477fe699d71ddc7344ae82e
zhouliupku <pkulz@pku.edu.cn>
2022-04-05 16:06:16 -0400

    Authorization for review
    add author to review and protect it

--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
+            author: '624c79e4ba45b221dae28680',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/45967841',
