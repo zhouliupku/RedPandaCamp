a3de58ddf56cf678861a86f32b0f96a5336c1d92
zhouliupku <pkulz@pku.edu.cn>
2022-04-05 14:31:39 -0400

    authentication
    register, login, logout

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
-// const catchAsync = require('./utils/catchAsync');
-// const { campgroundSchema, reviewSchema } = require('./schemas.js');
+
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-// const Campground = require('./models/campground');
-// const Review = require('./models/review');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/redpanda-camp', {
@@ -51,15 +53,24 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    // console.log(req.session)
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
